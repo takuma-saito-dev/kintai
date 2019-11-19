5f744d87a1e5148f0c23ed74517e8ff67421b21e
takuma <melufina88020@gmail.com>
2019-11-19 16:17:44 +0900

    1119コントローラー修正

--- demodemo/src/main/java/com/example/demodemo/HelloController.java
+++ demodemo/src/main/java/com/example/demodemo/HelloController.java
@@ -1,13 +1,38 @@
 package com.example.demodemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+//@Controller
+//public class HelloController {
+//
+//	@RequestMapping(value = "/hello")
+//	public String hello() {
+//		return "/index.html";
+//	}
+//	@RequestMapping(value = "/index")
+//	public String index() {
+//		return "/index.html";
+//	}
+//}
+
 
 @Controller
 public class HelloController {
+    @RequestMapping(value = "", method = {RequestMethod.GET, RequestMethod.POST})
+    public String home(Model model) {
+        model.addAttribute("hello", "Hello Thymeleaf!!");
+        return "/index.html";
+    }
 
-	@RequestMapping(value = "/hello")
-	private String hello() {
-		return "/index.html";
-	}
+    @RequestMapping(value = "/")
+    public ModelAndView index(ModelAndView mav) {
+        mav.addObject("hello", "Hello Thymeleaf!!");
+        mav.setViewName("index");
+        return mav;
+    }
+
 }
