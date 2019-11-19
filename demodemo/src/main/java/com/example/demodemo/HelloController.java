d87ce5bd53aa23bbd3751f8c91c5ded89f0c56f8
shun Ikeda <ikeshun@fsi.co.jp>
2019-11-19 16:02:13 +0900

    Controllerのmappingを一つ追加しました。

--- demodemo/src/main/java/com/example/demodemo/HelloController.java
+++ demodemo/src/main/java/com/example/demodemo/HelloController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = "/hello")
-	private String hello() {
-		return "/index.html";
+	@RequestMapping(value = "/kintai")
+	private String kintai() {
+		return "/register.html";
 	}
+	
+	@RequestMapping(value = "/list")
+	private String list() {
+		return "/kintai_List.html";
+	}
+	
 }
