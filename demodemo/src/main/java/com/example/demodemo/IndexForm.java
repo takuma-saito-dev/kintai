8e2aed7901f3f2c1c043d5780bdba7d7eae6e02f
takuma <melufina88020@gmail.com>
2019-11-20 11:12:32 +0900

    indexForm追加

---
+++ demodemo/src/main/java/com/example/demodemo/IndexForm.java
@@ -0,0 +1,29 @@
+package com.example.demodemo;
+
+
+public class IndexForm {
+
+    private String kiDate;
+	private String kiStart;
+    private String kiEnd;
+
+    public String getKiDate() {
+		return kiDate;
+	}
+	public void setKiDate(String kiDate) {
+		this.kiDate = kiDate;
+	}
+	public String getKiStart() {
+		return kiStart;
+	}
+	public void setKiStart(String kiStart) {
+		this.kiStart = kiStart;
+	}
+	public String getKiEnd() {
+		return kiEnd;
+	}
+	public void setKiEnd(String kiEnd) {
+		this.kiEnd = kiEnd;
+	}
+
+}
